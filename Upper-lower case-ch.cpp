a352cf072521dcd6ad179aeac9e0b0799c64741a
gaur9608 <143797789+gaur9608@users.noreply.github.com>
2024-09-22 01:30:18 +0530

    Q. Find Character lowercase OR Uppercase

--- Upper-lower case-ch.cpp
+++ Upper-lower case-ch.cpp
@@ -5,7 +5,7 @@ int main(){
     char ch;
     cout << "Enter a character: ";
     cin >> ch;
-    if (ch>='a' && ch<='z')
+    if (ch>='a' && ch<='z')// OR by ASCII value (97 to 122  ~ for samll letters ) OR (65 to 90 ~ for uppercase letter)
     {
         cout<<"Character is  of lower case";
     }else{
